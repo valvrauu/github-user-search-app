4813abeb398f92cb15d911dbf32462623c2bc350
Valter Oliveira <ubi00179@gmail.com>
2023-12-19 00:29:53 -0300

    :sparkles: feat: add new extends to tailwindcss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,34 @@ module.exports = {
       '2xl': '1.375rem',
       '3xl': '1.625rem',
     },
-    extend: {},
+    extend: {
+      screens: {
+        '2xs': '18.75em',
+        'xsm': '21.875em',
+      },
+      spacing: {
+        '18': '4.5rem',
+        '30': '7.5rem',
+        '35': '8.75rem',
+      },
+      colors: {
+        'azure-radiance': '#0079FF',
+        'polo-blue': '#90A4D4',
+        'lynch': '#697C9A',
+        'kashmir-blue': '#4B6A9B',
+        'ebony-clay': '#2B3442',
+        'zircon': '#F6F8FF',
+        'big-stone': '#141D2F',
+        'cloud-burst': '#1E2A47',
+        'white': '#FEFEFE',
+      },
+      fontFamily: {
+        'mono': ['Space Mono', 'monospace'],
+      },
+      boxShadow: {
+        'dark': '0 1rem 1.875rem -0.625rem rgba(70, 96, 187, 0.20)'
+      },
+    },
   },
   plugins: [],
 }
