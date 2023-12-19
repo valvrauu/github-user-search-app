2c85a0c411972fe2a494d5eb200563a46d4a71c0
Valter Oliveira <ubi00179@gmail.com>
2023-12-19 00:29:21 -0300

    :sparkles: feat: add custom font sizes to tailwindcss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   content: ["public/**/*.html"],
   darkMode: 'class',
   theme: {
+    fontSize: {
+      '2xs': '0.6875rem',
+      'xs': '0.8125rem',
+      'sm': '0.9375rem',
+      'base': '1rem',
+      'xl': '1.125rem',
+      '2xl': '1.375rem',
+      '3xl': '1.625rem',
+    },
     extend: {},
   },
   plugins: [],
